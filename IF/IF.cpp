97016e388c1c5e6e9669e35074fbb863c5ab9c0a
Patoo44 <91591600+Patoo44@users.noreply.github.com>
2021-10-27 08:38:59 +0200

    Add files via upload

---
+++ IF/IF.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int vek;
+	cout << "Zadaj svoj vek" << endl;
+	cin >> vek;
+	if (vek >= 18)
+		{
+			cout << "plnolety -- mozete vstupit" << endl;
+		}
+	else
+	{
+		cout << "neplnolety -- pristup zakazany" << endl;
+	}
+	}
+	
+
